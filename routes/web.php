aef4999c05c3fbf08d401eafaf25cd1fbb573ad1
Mqoey <mqoeyyy@gmail.com>
2022-11-20 16:24:40 +0200

    add superadmin model

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,31 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return route('login');
+    return redirect(route('customer.dashboard'));
 });
 
 Route::middleware('auth', 'customer')->group(function () {
-    Route::get('/dashboard', function () {
+    Route::get('/customerdashboard', function () {
         return view('customer.dashboard');
     })->name('customer.dashboard');
 });
 
 Route::middleware('auth', 'inspector')->group(function () {
-    Route::get('/dashboard', function () {
+    Route::get('/inspectordashboard', function () {
         return view('inspector.dashboard');
     })->name('inspector.dashboard');
 });
 
 Route::middleware('auth', 'inspectoradmin')->group(function () {
-    Route::get('/dashboard', function () {
+    Route::get('/inspectoradmindashboard', function () {
         return view('inspectoradmin.dashboard');
     })->name('inspectoradmin.dashboard');
 });
 
 Route::middleware('auth', 'superadmin')->group(function () {
-    Route::get('/dashboard', function () {
+    Route::get('/superadmindashboard', function () {
         return view('superadmin.dashboard');
     })->name('superadmin.dashboard');
 });
