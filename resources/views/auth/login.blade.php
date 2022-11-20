e363096565e5a766448eff7e3518b4c924d76be7
Mqoey <mqoeyyy@gmail.com>
2022-11-20 15:33:03 +0200

    add assets

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
@@ -55,3 +55,112 @@
         </form>
     </x-auth-card>
 </x-guest-layout>
+
+<!DOCTYPE html>
+<html lang="en"> --}}
+
+<!-- Mirrored from admin.pixelstrap.com/zeta/theme/login-sa-validation.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 19 Nov 2022 21:15:14 GMT -->
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description"
+        content="Zeta admin is super flexible, powerful, clean &amp; modern responsive bootstrap 5 admin template with unlimited possibilities.">
+    <meta name="keywords"
+        content="admin template, Zeta admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="author" content="pixelstrap">
+    <link rel="icon" href="assets/images/logo/favicon-icon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="assets/images/logo/favicon-icon.png" type="image/x-icon">
+    <title> DFSS | Login </title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com/">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin="">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
+        rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&amp;display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="assets/css/vendors/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/vendors/icofont.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/vendors/themify.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/vendors/flag-icon.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/vendors/feather-icon.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/vendors/sweetalert2.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/vendors/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link id="color" rel="stylesheet" href="assets/css/color-1.css" media="screen">
+    <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+</head>
+
+<body>
+    <div class="loader-wrapper">
+        <div class="loader">
+            <div class="loader-bar"></div>
+            <div class="loader-bar"></div>
+            <div class="loader-bar"></div>
+            <div class="loader-bar"></div>
+            <div class="loader-bar"></div>
+            <div class="loader-ball"></div>
+        </div>
+    </div>
+
+    <section>
+        <div class="container-fluid p-0">
+            <div class="row">
+                <div class="col-12">
+                    <div class="login-card">
+                        <x-auth-session-status class="mb-4" :status="session('status')" />
+                        <form class="theme-form login-form" method="POST" action="{{ route('login') }}">
+                            @csrf
+                            <h4>Login</h4>
+                            <h6>Welcome back! Log in to your account.</h6>
+                            <div class="form-group">
+                                <label>Email Address</label>
+                                <div class="input-group"><span class="input-group-text"><i
+                                            data-feather="user"></i></span>
+                                    <input class="form-control" name="email" type="email" required=""
+                                        placeholder="test@mail.com">
+                                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label>Password</label>
+                                <div class="input-group"><span class="input-group-text"><i
+                                            data-feather="lock"></i></span>
+                                    <input class="form-control" name="password" type="password" name="login[password]"
+                                        required="" placeholder="*********">
+                                    <div class="show-hide"><span class="show"> </span></div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="checkbox">
+                                    <input id="checkbox1" type="checkbox">
+                                    <label class="text-muted" for="checkbox1">Remember password</label>
+                                </div>
+                                @if (Route::has('password.request'))
+                                    <a class="link" href="{{ route('password.request') }}">Forgot password?</a>
+                                @endif
+                            </div>
+                            <div class="form-group">
+                                <button class="btn btn-primary btn-block" type="submit">Sign in</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="assets/js/jquery-3.5.1.min.js"></script>
+    <script src="assets/js/bootstrap/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/icons/feather-icon/feather.min.js"></script>
+    <script src="assets/js/icons/feather-icon/feather-icon.js"></script>
+    <script src="assets/js/config.js"></script>
+    <script src="assets/js/sweet-alert/sweetalert.min.js"></script>
+    <script src="assets/js/script.js"></script>
+</body>
+
+
+</html>
