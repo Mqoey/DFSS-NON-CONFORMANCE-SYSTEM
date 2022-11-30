774f8e40e4a72f5429bd23d29f2a675f4f182448
Mqondisi <57135780+Mqoey@users.noreply.github.com>
2022-11-30 04:06:18 +0200

    add new templete

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,103 +1,86 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-100">
 
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description"
-        content="LevierSoft Admin admin is super flexible, powerful, clean &amp; modern responsive bootstrap 5 LevierSoft Admin with unlimited possibilities.">
-    <meta name="keywords"
-        content="LevierSoft Admin, LevierSoft Admin LevierSoft Admin, dashboard template, flat LevierSoft Admin, responsive LevierSoft Admin, web app">
-    <meta name="author" content="leviersoft solution">
+    <meta name="keywords" content="admin, dashboard" />
+    <meta name="author" content="DexignZone" />
+    <meta name="robots" content="index, follow" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="tixia : tixia School Admission Admin  Bootstrap 5 Template" />
+    <meta property="og:title" content="tixia : tixia School Admission Admin  Bootstrap 5 Template" />
+    <meta property="og:description" content="tixia : tixia School Admission Admin  Bootstrap 5 Template" />
+    <meta property="og:image" content="social-image.png" />
+    <meta name="format-detection" content="telephone=no">
+    <title> DFSS | Login </title>
+    <!-- Favicon icon -->
     <link rel="icon" href="assets/images/logo/favicon-icon.png" type="image/x-icon">
     <link rel="shortcut icon" href="assets/images/logo/favicon-icon.png" type="image/x-icon">
-    <title> DFSS | Login </title>
+    <link href="assets/css/style.css" rel="stylesheet">
 
-    <link rel="preconnect" href="https://fonts.googleapis.com/">
-    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin="">
-    <link
-        href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
-        rel="stylesheet">
-    <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&amp;display=swap"
-        rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="assets/css/vendors/font-awesome.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/vendors/icofont.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/vendors/themify.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/vendors/flag-icon.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/vendors/feather-icon.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/vendors/sweetalert2.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/vendors/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-    <link id="color" rel="stylesheet" href="assets/css/color-1.css" media="screen">
-    <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
 </head>
 
-<body>
-    <div class="loader-wrapper">
-        <div class="loader">
-            <div class="loader-bar"></div>
-            <div class="loader-bar"></div>
-            <div class="loader-bar"></div>
-            <div class="loader-bar"></div>
-            <div class="loader-bar"></div>
-            <div class="loader-ball"></div>
-        </div>
-    </div>
-
-    <section>
-        <div class="container-fluid p-0">
-            <div class="row">
-                <div class="col-12">
-                    <div class="login-card">
-                        <form class="theme-form login-form" method="POST" action="{{ route('login') }}">
-                            @csrf
-                            <h4>Login</h4>
-                            <h6>Welcome back! Log in to your account.</h6>
-                            <x-input-error :messages="$errors->get('email')" style="color: red" class="mt-2" />
-                            <div class="form-group">
-                                <label>Email Address</label>
-                                <div class="input-group"><span class="input-group-text"><i
-                                            data-feather="user"></i></span>
-                                    <input class="form-control" name="email" type="email" required=""
-                                        placeholder="email...">
+<body class="vh-100">
+    <div class="authincation h-100">
+        <div class="container h-100">
+            <div class="row justify-content-center h-100 align-items-center">
+                <div class="col-md-6">
+                    <div class="authincation-content">
+                        <div class="row no-gutters">
+                            <div class="col-xl-12">
+                                <div class="auth-form">
+                                    <div class="text-center mb-3">
+                                        <a href="#"><img src="assets/images/logo.png" width="70dp"
+                                                height="70dp" alt=""></a>
+                                    </div>
+                                    <h4 class="text-center mb-4">Sign in your account</h4>
+                                    <form class="theme-form login-form" method="POST" action="{{ route('login') }}">
+                                        @csrf
+                                        <div class="form-group">
+                                            <label class="mb-1"><strong>Email</strong></label>
+                                            <input type="email" class="form-control" name="email"
+                                                placeholder="user@mail.com">
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="mb-1"><strong>Password</strong></label>
+                                            <input type="password" class="form-control" name="password"
+                                                placeholder="password">
+                                        </div>
+                                        <div class="form-row d-flex justify-content-between mt-4 mb-2">
+                                            <div class="form-group">
+                                                <div class="form-check custom-checkbox ms-1">
+                                                    <input type="checkbox" class="form-check-input"
+                                                        id="basic_checkbox_1">
+                                                    <label class="form-check-label" for="basic_checkbox_1">Remember me
+                                                    </label>
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                @if (Route::has('password.request'))
+                                                    <a href="{{ route('password.request') }}">Forgot password?</a>
+                                                @endif
+                                            </div>
+                                        </div>
+                                        <div class="text-center">
+                                            <button type="submit" class="btn btn-primary btn-block">Sign Me
+                                                In</button>
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
-                            <div class="form-group">
-                                <label>Password</label>
-                                <div class="input-group"><span class="input-group-text"><i
-                                            data-feather="lock"></i></span>
-                                    <input class="form-control" name="password" type="password" name="login[password]"
-                                        required="" placeholder="password...">
-                                    <div class="show-hide"><span class="show"> </span></div>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <div class="checkbox">
-                                    <input id="checkbox1" type="checkbox">
-                                    <label class="text-muted" for="checkbox1">Remember password</label>
-                                </div>
-                                @if (Route::has('password.request'))
-                                    <a class="link" href="{{ route('password.request') }}">Forgot password?</a>
-                                @endif
-                            </div>
-                            <div class="form-group">
-                                <button class="btn btn-primary btn-block" type="submit">Sign in</button>
-                            </div>
-                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
 
-    <script src="assets/js/jquery-3.5.1.min.js"></script>
-    <script src="assets/js/bootstrap/bootstrap.bundle.min.js"></script>
-    <script src="assets/js/icons/feather-icon/feather.min.js"></script>
-    <script src="assets/js/icons/feather-icon/feather-icon.js"></script>
-    <script src="assets/js/config.js"></script>
-    <script src="assets/js/sweet-alert/sweetalert.min.js"></script>
-    <script src="assets/js/script.js"></script>
+    <script src="vendor/global/global.min.js"></script>
+    <script src="vendor/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
+    <script src="js/custom.min.js"></script>
+    <script src="js/deznav-init.js"></script>
+
 </body>
+
 </html>
