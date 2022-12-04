c545897c9f421a444ce9ae5f094cc962aa48e2dd
Mqondisi <57135780+Mqoey@users.noreply.github.com>
2022-12-04 13:48:15 +0200

    add reports file

--- resources/views/components/navs/superadmin.blade.php
+++ resources/views/components/navs/superadmin.blade.php
@@ -5,7 +5,7 @@
                 <span class="nav-text">Dashboard</span>
             </a>
         </li>
-        <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+        <li><a class="has-arrow ai-icon" href="#" aria-expanded="false">
                 <i class="flaticon-381-heart"></i>
                 <span class="nav-text">Customers</span>
             </a>
@@ -14,7 +14,7 @@
                 <li><a href="{{ route('customer.create') }}">Create Customer</a></li>
             </ul>
         </li>
-        <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+        <li><a class="has-arrow ai-icon" href="#" aria-expanded="false">
                 <i class="flaticon-381-television"></i>
                 <span class="nav-text">Inspectors</span>
             </a>
@@ -23,7 +23,7 @@
                 <li><a href="{{ route('inspector.create') }}">Create Inspector</a></li>
             </ul>
         </li>
-        <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+        <li><a class="has-arrow ai-icon" href="#" aria-expanded="false">
                 <i class="flaticon-381-controls-3"></i>
                 <span class="nav-text">Inspector Admins</span>
             </a>
@@ -32,7 +32,7 @@
                 <li><a href="{{ route('inspectoradmin.create') }}">Create Inspector Admin</a></li>
             </ul>
         </li>
-        <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+        <li><a class="has-arrow ai-icon" href="#" aria-expanded="false">
                 <i class="flaticon-381-notepad"></i>
                 <span class="nav-text">Non-Conformative</span>
             </a>
@@ -42,13 +42,23 @@
                 </ul>
             </ul>
         </li>
-        <li><a class="has-arrow ai-icon" href="javascript:void()" aria-expanded="false">
+        <li><a class="has-arrow ai-icon" href="#" aria-expanded="false">
                 <i class="flaticon-381-network"></i>
                 <span class="nav-text">Users</span>
             </a>
             <ul aria-expanded="false">
                 <li><a href="{{ route('user.index') }}">All Users</a></li>
                 <li><a href="{{ route('role.index') }}">View Roles</a></li>
+            </ul>
+        </li>
+        <li><a class="has-arrow ai-icon" href="#" aria-expanded="false">
+                <i class="flaticon-381-network"></i>
+                <span class="nav-text">Roperts</span>
+            </a>
+            <ul aria-expanded="false">
+                <li><a href="#">Daily</a></li>
+                <li><a href="#">Weekly</a></li>
+                <li><a href="#">Monthly</a></li>
             </ul>
         </li>
     </ul>
